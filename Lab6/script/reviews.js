3321ad8c011a0bf37732a003014d938a579b41e1
Danila Lobov <89832616+ReDZiLLa7@users.noreply.github.com>
2023-12-18 13:26:59 +0300

    Done

--- Lab6/script/reviews.js
+++ Lab6/script/reviews.js
@@ -1,19 +1,34 @@
+// Получаем ссылку на элемент с классом "clients-reviews"
 const commentsWrapper = document.querySelector(".clients-reviews");
+const commentTemplate = document.getElementById('commentTemplate');
+// Изменение параметра positionClass
+toastr.options.positionClass = 'toast-bottom-left';
 
 function renderComments(comments) {
+    hidePreloader()
     comments.forEach(item => {
-        const comment = document.createElement("div");
-        comment.innerHTML = `
-            <div class="comment-person">
-                <div class="comment-name">Имя: ${item.name}</div>
-                <div class="comment-body">Комментарий: ${item.body}</div>
-            </div>
-        `;
-        commentsWrapper.insertAdjacentHTML("beforeend", comment.innerHTML);
+        // Клонируем шаблон комментария
+        const comment = document.importNode(commentTemplate.content, true);
+
+        // Заполняем значения комментария
+        comment.querySelector('.comment-name-value').textContent = item.name;
+        comment.querySelector('.comment-body-value').textContent = item.body;
+
+        // Добавляем комментарий в обертку
+        commentsWrapper.appendChild(comment);
     });
 }
 
-// Показать уведомление
+function hidePreloader() {
+    // Найти элемент прелоадера
+    const preloader = document.querySelector(".preloader");
+
+    // Скрыть прелоадер (изменить стиль или удалить элемент)
+    preloader.style.display = "none";
+    // preloader.remove(); // или удалить элемент из DOM
+}
+
+// Объявляется функция showNotification, которая использует библиотеку toastr для отображения уведомлений
 const showNotification = (message, type = 'success') => {
     toastr[type](message);
 };
@@ -21,15 +36,14 @@ const showNotification = (message, type = 'success') => {
 // Показать loader перед запросом
 showNotification('Loading...');
 
-fetch('https://jsonplaceholder.typicode.com/comments')
-    .then(response => response.json())
-    .then(allComments => {
-        // Получаем 10 случайных комментариев
-        const randomComments = Array.from({ length: 10 }, () => {
-            const randomIndex = Math.floor(Math.random() * allComments.length);
-            return allComments[randomIndex];
-        });
+const randomId = Math.floor(Math.random() * 100) + 1;
 
+// Выполняется HTTP-запрос к API JSONPlaceholder для получения списка комментариев
+fetch(`https://jsonplaceholder.typicode.com/posts/${randomId}/comments`)
+    // Обработка успешного ответа, преобразование его в формат JSON
+    .then(response => response.json())
+    // Обработка JSON-данных (всех комментариев), выполнение следующего кода при успешном выполнении
+    .then(randomComments => {
         renderComments(randomComments);
         // Скрыть loader после успешного запроса
         showNotification('Data loaded successfully');
@@ -38,3 +52,5 @@ fetch('https://jsonplaceholder.typicode.com/comments')
         console.error('Error:', error);
         showNotification(`Error: ${error}`, 'error');
     });
+
+// Прочитать про обработку микрозадач ( catch )
